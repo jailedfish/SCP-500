71c21300f2c2faa5ae41a20ee97390e6bca77886
cabup <pass@localhost>
2024-03-29 19:02:14 +0300

    Добавьте файлы проекта.

---
+++ SCP-500/Config.cs
@@ -0,0 +1,9 @@
+﻿using Exiled.API.Interfaces;
+
+
+namespace SCP_500.Conf {
+    public class Config : IConfig {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+    }
+}
---
+++ SCP-500/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API;
+using Exiled.API.Features;
+using SCP_500.Handlers;
+using SCP_500.Conf;
+
+namespace SCP_500 {
+    public class SCP500 : Plugin<Config> {
+        public override string Author => "[Девопёс] Сэр Енот";
+        public override string Name => "SCP 500 role";
+        public MainHandler handler;
+        public override void OnEnabled() {
+            handler = new MainHandler();
+
+            
+        }
+    }
+}
